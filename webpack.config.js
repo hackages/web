b07664e52277e84842e60c3f05cdaa87da80d682
davy engone <davy@philos.io>
2015-12-18 22:46:23 +0100

    changed entry point

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 
 module.exports = {
   devtool: 'source-map',
-  entry: './index.js',
+  entry: './app.js',
   output: {
     path: path.join(__dirname, 'www'),
     filanme: 'iterators',
